dddb6daf6e9674627be5e2e725e2f342b5980599
Harshvardhan Arora <swiftsimplified@gmail.com>
2025-03-07 11:31:33 +0000

    LC-TwoSum-Improvement

--- DSA.playground/Pages/LC-TwoSum.xcplaygroundpage/Contents.swift
+++ DSA.playground/Pages/LC-TwoSum.xcplaygroundpage/Contents.swift
@@ -3,34 +3,17 @@
 import Foundation
 
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    var output: [Int] = []
+    var numsDict = [Int: Int]()
 
-    var numsDict = [Int: [Int]]()
     for (index, num) in nums.enumerated() {
-        var arr = numsDict[num] ?? []
-        arr.append(index)
-        numsDict[num] = arr
-    }
-
-    print(numsDict)
-
-    for key in numsDict.keys {
-        let firstElementIndex = numsDict[key]!.first!
-        let diff = target - key
-
-        if diff == key {
-            if let arr = numsDict[key], arr.count >= 2 {
-                output = [arr[0], arr[1]]
-                break
-            }
-        } else if let secondElementIndex = numsDict[diff]?.first {
-            output.append(firstElementIndex)
-            output.append(secondElementIndex)
-            break
+        if let secondIndex = numsDict[target - num] {
+            return [index, secondIndex]
+        } else {
+            numsDict[num] = index
         }
     }
 
-    return output
+    return []
 }
 
 let input = [3, 2, 4]
