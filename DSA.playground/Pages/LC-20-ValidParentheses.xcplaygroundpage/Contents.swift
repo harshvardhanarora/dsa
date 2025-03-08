ddba0fa70c4b4c37d8065b6174cfdc07bfb416d6
Harshvardhan Arora <swiftsimplified@gmail.com>
2025-03-08 15:54:22 +0000

    LC-ValidParentheses

---
+++ DSA.playground/Pages/LC-20-ValidParentheses.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//
+//  LC-20-ValidParentheses
+//
+//  Created by Harshvardhan Arora on 08/03/2025.
+//
+// https://leetcode.com/problems/valid-parentheses/
+//
+
+func isValid(_ s: String) -> Bool {
+    var storage = [Character]()
+
+    for char in s {
+        if char == "(" || char == "{" || char == "[" {
+            storage.append(char)
+        } else {
+            if let pop = storage.popLast() {
+                switch pop {
+                case "(": if char != ")" { return false }
+                case "{": if char != "}" { return false }
+                case "[": if char != "]" { return false }
+                default: return false
+                }
+            } else {
+                return false
+            }
+        }
+    }
+
+    if storage.isEmpty {
+        return true
+    } else {
+        return false
+    }
+}
+
+let input = "([])"
+print(isValid(input))
