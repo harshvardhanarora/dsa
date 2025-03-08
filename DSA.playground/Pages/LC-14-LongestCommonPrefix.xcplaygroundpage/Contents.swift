44b9a05ded38f6bbc9a6d8105ea600f898fc0021
Harshvardhan Arora <swiftsimplified@gmail.com>
2025-03-08 15:39:45 +0000

    LC-LongestCommonPrefix

---
+++ DSA.playground/Pages/LC-14-LongestCommonPrefix.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+//
+//  LC-14-LongestCommonPrefix
+//
+//  Created by Harshvardhan Arora on 07/03/2025.
+//
+// https://leetcode.com/problems/longest-common-prefix/
+//
+
+func longestCommonPrefix(_ strs: [String]) -> String {
+    var output = ""
+    var shouldContinue = true
+    var index = 0
+
+    var arr = strs
+    arr.sort(by: { $0.count < $1.count })
+
+    while(index < arr[0].count) {
+        var shouldAppend = true
+        let char = arr[0][index]
+
+        for str in arr {
+            if str[index] != char {
+                shouldAppend = false
+                break
+            }
+        }
+        if shouldAppend {
+            output.append(char)
+            index += 1;
+        } else {
+            break
+        }
+    }
+
+    return output
+}
+
+let input = ["flower","flow","flight"]
+print(longestCommonPrefix(input))
