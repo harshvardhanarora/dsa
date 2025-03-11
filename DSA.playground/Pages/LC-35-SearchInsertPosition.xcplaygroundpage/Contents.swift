7e64a80a7cb4102502be04ecbbf681da9addc1f5
Harshvardhan Arora <swiftsimplified@gmail.com>
2025-03-11 21:31:49 +0000

    LC-SearchInsertPosition-Improvement

--- DSA.playground/Pages/LC-35-SearchInsertPosition.xcplaygroundpage/Contents.swift
+++ DSA.playground/Pages/LC-35-SearchInsertPosition.xcplaygroundpage/Contents.swift
@@ -20,6 +20,33 @@ func searchInsert_1(_ nums: [Int], _ target: Int) -> Int {
     return nums.count
 }
 
+func searchInsert_2(_ nums: [Int], _ target: Int) -> Int {
+        if target <= nums[0] {
+            return 0
+        } else if target == nums[nums.count - 1] {
+            return nums.count - 1
+        } else if target > nums[nums.count - 1] {
+            return nums.count
+        }
+
+        let size = nums.count
+        var greaterThanIndex = 0
+        var lessThanIndex = nums.count - 1
+
+        while(lessThanIndex - greaterThanIndex > 1) {
+            let index = greaterThanIndex + (lessThanIndex - greaterThanIndex + 1) / 2
+            if nums[index] == target {
+                return index
+            } else if nums[index] > target {
+                lessThanIndex = index
+            } else {
+                greaterThanIndex = index
+            }
+        }
+
+        return lessThanIndex
+    }
+
 let nums = [1, 4, 6, 8]
 let target = 9
 
