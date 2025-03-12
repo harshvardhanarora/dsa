14ee9d51cd14f2b41e674804f612cf9881e9f683
Harshvardhan Arora <swiftsimplified@gmail.com>
2025-03-12 09:26:54 +0000

    LC-NumberOfSubstringsContainingAllThreeCharacters-Improvement

--- DSA.playground/Pages/LC-1358-NumberOfSubstringsContainingAllThreeCharacters.xcplaygroundpage/Contents.swift
+++ DSA.playground/Pages/LC-1358-NumberOfSubstringsContainingAllThreeCharacters.xcplaygroundpage/Contents.swift
@@ -7,6 +7,8 @@
 //
 
 func numberOfSubstrings_1(_ s: String) -> Int {
+    // This is the best solution by converting the string to array of chars
+    let s = Array(s)
     let length = s.count
     if length < 3 {
         return 0
@@ -114,5 +116,67 @@ func numberOfSubstrings_2(_ s: String) -> Int {
     return count
 }
 
+func numberOfSubstrings_3(_ s: String) -> Int {
+    var length = s.count
+
+    var aHead: ListNode? = nil
+    var aNode = aHead
+    var bHead: ListNode? = nil
+    var bNode = bHead
+    var cHead: ListNode? = nil
+    var cNode = cHead
+
+    var count = 0
+
+    for index in 0..<length {
+        switch s[index] {
+        case "a":
+            let next = ListNode(index)
+            if aHead == nil {
+                aHead = next
+                aNode = aHead
+            } else {
+                aNode!.next = next
+                aNode = aNode!.next
+            }
+        case "b":
+            let next = ListNode(index)
+            if bHead == nil {
+                bHead = next
+                bNode = bHead
+            } else {
+                bNode!.next = next
+                bNode = bNode!.next
+            }
+        default:
+            let next = ListNode(index)
+            if cHead == nil {
+                cHead = next
+                cNode = cHead
+            } else {
+                cNode!.next = next
+                cNode = cNode!.next
+            }
+        }
+
+        while(aHead != nil && bHead != nil && cHead != nil) {
+            var max = max(aHead!.value, max(bHead!.value, cHead!.value))
+            count += length - max
+
+            var min = min(aHead!.value, min(bHead!.value, cHead!.value))
+            switch min {
+            case aHead!.value:
+                aHead = aHead!.next
+            case bHead!.value:
+                bHead = bHead!.next
+            default:
+                cHead = cHead!.next
+            }
+        }
+    }
+
+    return count
+}
+
 let input = "abcabc"
-print(numberOfSubstrings_2(input))
+print(numberOfSubstrings_1(input))
