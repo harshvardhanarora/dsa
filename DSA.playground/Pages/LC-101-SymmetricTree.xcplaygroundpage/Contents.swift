b18c52f64b3750e5b12a2534c082ab513f552908
Harshvardhan Arora <swiftsimplified@gmail.com>
2025-04-12 10:38:56 +0100

    LC-SymmetricTree-Improvement

--- DSA.playground/Pages/LC-101-SymmetricTree.xcplaygroundpage/Contents.swift
+++ DSA.playground/Pages/LC-101-SymmetricTree.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@
 //  https://leetcode.com/problems/symmetric-tree/description/
 //
 
-func isSymmetric(_ root: TreeNode?) -> Bool {
+func isSymmetric_1(_ root: TreeNode?) -> Bool {
     var arr1 = [Int?]()
     var arr2 = [Int?]()
     preorderTraversal(root)
@@ -37,4 +37,14 @@ func isSymmetric(_ root: TreeNode?) -> Bool {
     print(arr1)
     print(arr2)
     return arr1 == arr2
+}
+
+func isSymmetric_2(_ root: TreeNode?) -> Bool {
+    return isSymmetric(root, root)
+
+    func isSymmetric(_ left: TreeNode?, _ right: TreeNode?) -> Bool {
+        if left == nil && right == nil { return true }
+
+        return left?.val == right?.val && isSymmetric(left?.left, right?.right) && isSymmetric(left?.right, right?.left)
+    }
 }
